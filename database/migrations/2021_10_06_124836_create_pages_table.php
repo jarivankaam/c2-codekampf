8d50f876ba6b2ca9c0e3c6ed8a977728f561cd1d
jaap <jaapelst@gmail.com>
2021-10-06 14:58:56 +0200

    added migrations

---
+++ database/migrations/2021_10_06_124836_create_pages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("category_id");
+            $table->foreign("category_id")->references("id")->on("page");
+            $table->text("name");
+            $table->string("color", 10)->default("#FFF");
+            $table->string("slug", 15)->nullable(false);
+            $table->text("title");
+            $table->text("content");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
