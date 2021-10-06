11a47a446d8c12c7d9f1deb2490c7da339d9c3ff
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2021-10-06 14:43:53 +0200

    Added sass, css and homepage

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,11 @@
+
+<meta charset="utf-8">
+
+
+
+<meta name="language" content="en">
+<meta name="google-site-verification" content="Fdb3h6i9VSbRysXMwdteA4E2tZnedlygCcIyJBADqKo" />
+
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+
