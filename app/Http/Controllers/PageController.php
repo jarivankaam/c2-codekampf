ba57e2281831564b520b1a0242a07078baf36312
jaap <jaapelst@gmail.com>
2021-10-11 14:00:40 +0200

    added all get functionality

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Page;
 use Illuminate\Http\Request;
+use Parsedown;
 
 class PageController extends Controller
 {
@@ -15,5 +17,35 @@ class PageController extends Controller
     }
     function store(){
 
+    }
+
+    function get(Request $request, string $category_slug, string $page_slug) {
+        $cat = $this->getCategory($category_slug);
+
+        if ($cat == null) {
+            abort(404);
+            return null;
+        }
+
+        $page = $this->getPage($cat["id"], $page_slug);
+
+        if ($page == null) {
+            abort(404);
+            return null;
+        }
+
+        $parseDown = new Parsedown();
+        $content = $parseDown->text($page["content"]);
+
+
+        return ;
+    }
+
+    function getCategory(string $slug): ?Category {
+        return Category::query()->where("slug", "=", $slug)->get()->first();
+    }
+
+    function getPage($catId, string $slug): ?Page {
+        return Page::query()->where("category_id", "=", $catId)->where("slug", "=", $slug)->get()->first();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/{category}/{page}", [PageController::class, "get"])->name("page");
 Route::get('create', function () {
     return view('page/create');
 });
