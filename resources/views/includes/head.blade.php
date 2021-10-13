625d057c0b19b5cca82606a10f88d67aeba7c53f
Faab007NL <mail@fabiandingemans.nl>
2021-10-13 15:02:04 +0200

    Fixed head on page pages

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,11 +1,5 @@
-
 <meta charset="utf-8">
-
-
-
 <meta name="language" content="en">
 <meta name="google-site-verification" content="Fdb3h6i9VSbRysXMwdteA4E2tZnedlygCcIyJBADqKo" />
-
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-
+<link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
