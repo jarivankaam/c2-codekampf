d037f43f7040d5c5fcda518d3df87e8f899f366d
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2021-10-14 11:40:32 +0200

    changed header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,11 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <div class="header">
 
-    <a href="/">Home</a>
-    <a href="#">Pages</a>
-    <h2>C2-CodeCamp</h2>
-    <a href="/page/create">page create</a>
-    <a href="#">POC</a>
+
+    <h2><a href="/">C2-CodeCamp</a></h2>
+
 </div>
 
 </html>
