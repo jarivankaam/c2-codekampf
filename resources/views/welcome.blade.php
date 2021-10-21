ebba3cbb08d4c6d5ff10807756d99e423411d699
Florisvanbezooijen <D286786@edu.rocwb.nl>
2021-10-21 11:31:13 +0200

    maps toegevoegd en het werkt kut jaap!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,8 +36,27 @@
                     <h3>Contact Us</h3>
                     <p>Telefoonnummer: 076-80934847</p>
                     <p>Addres: Koestraat 1 Den Bosch</p>
+                    
+                    <div id="googleMap" style="width:75%;height:400px;"></div>  
 
-                    <img src="{{ asset('img/koestraat.png') }}" alt="plattegrond">
+                    <script>
+                        function myMap() {
+                        var mapProp= {
+                        center:new google.maps.LatLng(51.6973624,5.2914112),
+                        zoom:17,
+                        };
+                        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                        new google.maps.Marker({
+                        position: {lat: 51.6973624, lng: 5.2914112},
+                        map,
+                        title: "Hello World!",
+                        });
+                        }
+
+
+                    </script>
+                    
+                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuUMx5QRvBmNC5Dv3o1Jqr0UlxRn8xyuw&callback=myMap"></script>
                 </div>
                 <div class="about">
                     <h3>About us</h3>
