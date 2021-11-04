1d63249540782a9423a34fca8454db5034c267ff
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-11-04 12:00:00 +0100

    Alpha Male Dropdown

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class homeController extends Controller
 {
     public static function categoryInclude() {
+
         $categories = Category::all()->all();
         return view("includes/header")
             ->with("categories", $categories);
--- app/Http/Controllers/HomeController2.php
+++ app/Http/Controllers/HomeController2.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController2 extends Controller
 {
     public static function categoryInclude() {
-        $categories = Category::all()->all();
+        $categories = Category::all()->sortBy("title", SORT_NATURAL | SORT_FLAG_CASE);
         return view("includes/header")
             ->with("categories", $categories);
     }
